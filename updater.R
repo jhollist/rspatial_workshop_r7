6efc4e52ebd96d25ab438653144963d25c8e36fa
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-10-10 12:34:50 -0400

    'update'

---
+++ updater.R
@@ -0,0 +1,14 @@
+while(TRUE){
+  
+  Sys.sleep(10)
+  if(!dir.exists(here::here(".git"))){
+    setwd(here::here())
+    prj_name <- stringr::str_extract(here::here(), "[\\w]+$")
+    remote_url <- paste0("https://github.com/jhollist/", prj_name)
+    system("git init")
+    system(paste("git remote add origin", remote_url))
+  }
+  system("git add -A")
+  system("git commit -m 'update'")
+  system("git push origin master")
+}
